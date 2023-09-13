35eed1815d4a8635dba2bc571151a9976bfaa108
Anderson Costa <andersonfdc47@gmail.com>
2023-09-13 13:14:26 -0300

    add input_print

---
+++ MOD_1-Fundamentos/input_print.py
@@ -0,0 +1,6 @@
+name = input("What's your first name: ")
+last_name = input("What's your last name: ")
+
+print(name, last_name)
+print(name, last_name, end="...\n")
+print(name, last_name, sep="#")

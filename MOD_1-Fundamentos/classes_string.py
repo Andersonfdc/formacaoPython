9d59b76eea6c279361f96afe887637421603f50a
Anderson Costa <andersonfdc47@gmail.com>
2023-09-14 14:03:13 -0300

    add formatacao de string

---
+++ MOD_1-Fundamentos/classes_string.py
@@ -0,0 +1,18 @@
+name = "pYtHoN"
+
+print(name.upper())  # maiúsculo
+print(name.lower())  # minúsculo
+print(name.title())  # primeira letra maiúscula
+
+name = "     Python"
+
+print(name.strip())  # Remove espeços da direita e esquerda
+print(name.lstrip())  # Remove espeços da esquerda
+print(name.rstrip())  # Remove espeços da direita
+
+menseger = f''' 
+ lorenipsu lorenipsu lorenipsu
+ lorenipsu
+ lorenipsu lorenipsu
+'''
+print(menseger)

82648e6772230844178340616c8767cbaf56b040
Anderson Costa <andersonfdc47@gmail.com>
2023-10-03 16:50:06 -0300

    Portifólio concluido

--- Django/Blog_seg_info/galeria/models.py
+++ Django/Blog_seg_info/galeria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100,null=False,blank=False)
+    legenda = models.CharField(max_length=150,null=False,blank=False)
+    descricao = models.TextField(null=False,blank=False)
+    link = models.CharField(max_length=150, null = True)
+    foto = models.ImageField(upload_to= "fotos/%Y/%m/%d/", blank=True)
+    publicada = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+
--- Django/Blog_seg_info/galeria/views.py
+++ Django/Blog_seg_info/galeria/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    fotografias =  Fotografia.objects.filter(publicada=True)
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render (request,'galeria/imagem.html', {'titulo':'Imagem'})
+
+
+def buscar (request):
+    fotografias =  Fotografia.objects.filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(legenda__icontains= nome_a_buscar) or fotografias.filter(nome__icontains= nome_a_buscar)
+
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
